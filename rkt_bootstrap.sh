bff69c5ec7c74371c67c7d96e28b8e599a95c23c
Lothar Schulz <lothar.sch@gmx.de>
2015-02-13 23:54:32 +0100

    rkt v0.3.2

--- rkt_bootstrap.sh
+++ rkt_bootstrap.sh
@@ -5,6 +5,6 @@ sudo apt-get install unattended-upgrades
 sudo apt-get install language-pack-en
 #apt-get upgrade -y # enable this one if you want all packages up to date
 
-wget https://github.com/coreos/rocket/releases/download/v0.3.1/rocket-v0.3.1.tar.gz
-tar xzvf rocket-v0.3.1.tar.gz
-cd rocket-v0.3.1
+wget https://github.com/coreos/rocket/releases/download/v0.3.2/rocket-v0.3.2.tar.gz
+tar xzvf rocket-v0.3.2.tar.gz
+cd rocket-v0.3.2
