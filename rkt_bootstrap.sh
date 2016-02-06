170fdd041987840f76cfae06f2dabe08221731a3
lothar schulz <lotharschulz@users.noreply.github.com>
2016-02-06 22:22:42 +0100

    update v1.0.0

--- rkt_bootstrap.sh
+++ rkt_bootstrap.sh
@@ -5,6 +5,6 @@ sudo apt-get install unattended-upgrades
 sudo apt-get install language-pack-en
 #apt-get upgrade -y # enable this one if you want all packages up to date
 
-wget https://github.com/coreos/rkt/releases/download/v0.12.0/rkt-v0.12.0.tar.gz
-tar xzvf rkt-v0.12.0.tar.gz
-cd rkt-v0.12.0 
+wget https://github.com/coreos/rkt/releases/download/v1.0.0/rkt-v1.0.0.tar.gz
+tar xzvf rkt-v1.0.0.tar.gz
+cd rkt-v1.0.0
